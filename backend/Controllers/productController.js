0e75586beee7a2a00c7dcb74a24e9f12e749303d
av3n93rz <63319813+av3n93rz@users.noreply.github.com>
2020-10-26 10:41:42 +0100

    Add files via upload

--- backend/Controllers/productController.js
+++ backend/Controllers/productController.js
@@ -1,4 +1,4 @@
-const Product = require('../models/productModel.js')
+const Product = require('../models/productModel')
 const asyncHandler = require('express-async-handler')
 
 exports.getAllProducts = asyncHandler (async (req, res) => {
