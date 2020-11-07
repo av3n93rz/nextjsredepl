d42db2e74cdb27b341353dbf0fa758502f39a29d
av3n93rz <63319813+av3n93rz@users.noreply.github.com>
2020-11-07 21:03:36 +0100

    Add files via upload

--- src/theme.js
+++ src/theme.js
@@ -5,16 +5,33 @@ import { red } from '@material-ui/core/colors';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#fdc54a',
     },
     secondary: {
+      main: '#234f72',
+    },
+    third: {
       main: '#171d23',
+    },
+    fourth: {
+      main: '#556cd6',
     },
     error: {
       main: red.A400,
     },
     background: {
       default: '#fff',
+    },
+  },
+  breakpoints: {
+    values: {
+      xxs: 0,
+      xs: 460,
+      sm: 600,
+      smd: 750,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
     },
   },
 });
